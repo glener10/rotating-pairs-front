f24d3b5b220ec89185ebc024b3825e1ab45f0bb3
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2023-09-05 19:28:05 -0300

    refactor: refactoring pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { Drawer } from '@/components/organisms/Drawer';
+import styles from '@/styles/Home.module.css';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Drawer of Pair Programming</title>
+        <meta name="description" content="Drawer of Pair Programming" />
+        <link rel="icon" href="/iconDrawPairProgramming.svg" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.center}>
+          <Drawer />
+        </div>
+
+        <div className={styles.grid}>
+          <p>Rodapé</p>
+        </div>
+      </main>
+    </>
+  );
+}
