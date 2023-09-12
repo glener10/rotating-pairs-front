4c1ff1e1a07f33966d596087c28a4df09fc49c4e
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2023-09-12 19:18:38 -0300

    upload arqs to working with json

---
+++ src/components/atoms/ButtonCombinations/staticLogicReadDrawerJSON.tsx
@@ -0,0 +1,7 @@
+import { ISprint } from '@/interfaces/ISprint';
+
+export const staticLogicGenerateCombinations = (inputNamesInArray: string[]): ISprint[] => {
+  console.log(inputNamesInArray);
+  const sprints: ISprint[] = [];
+  return sprints;
+};
