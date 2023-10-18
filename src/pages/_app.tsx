c6e84e40c2773344823b07025c68af37e17a675f
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2023-10-18 19:39:02 -0300

    feat: take theme with user prefer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,35 @@ import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+export const getThemePreference = (): string => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('themePreference') || 'dark';
+  }
+
+  return 'dark';
+};
+
+export const setThemePreference = (theme: string): void => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('themePreference', theme);
+  }
+};
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const [theme, setTheme] = useState('dark');
 
+  useEffect(() => {
+    setTheme(getThemePreference());
+  }, []);
+
+  useEffect(() => {
+    setThemePreference(theme);
+  }, [theme]);
+
   return (
-    <Theme appearance={theme == 'light' ? 'light' : 'dark'}>
+    <Theme appearance={theme === 'light' ? 'light' : 'dark'}>
       <TooltipProvider>
         <Layout theme={theme} setTheme={setTheme}>
           <Analytics />
