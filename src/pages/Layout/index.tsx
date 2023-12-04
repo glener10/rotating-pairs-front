3d7149e21244a2035c34e127cd0beb55b675487f
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2023-12-04 11:04:50 -0300

    fix: removing trash about About Modal

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,7 +2,7 @@ import { Ad } from '@/components/atoms/Ad';
 import useResponsive from '@/hooks/useResponsive';
 import { Footer } from '@/pages/Layout/Footer';
 import { Header } from '@/pages/Layout/Header';
-import { Dispatch, ReactNode, SetStateAction, useState } from 'react';
+import { Dispatch, ReactNode, SetStateAction } from 'react';
 
 type LayoutProps = {
   theme: string;
@@ -12,18 +12,12 @@ type LayoutProps = {
 
 export const Layout = (props: LayoutProps): JSX.Element => {
   const { theme, setTheme, children } = props;
-  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
 
   const breakpoint = useResponsive();
 
   return (
     <>
-      <Header
-        theme={theme}
-        setTheme={setTheme}
-        isAboutModalOpen={isAboutModalOpen}
-        setIsAboutModalOpen={setIsAboutModalOpen}
-      />
+      <Header theme={theme} setTheme={setTheme} />
 
       {breakpoint && breakpoint == 'desktop' ? (
         <div style={{ display: 'flex' }}>
@@ -39,7 +33,7 @@ export const Layout = (props: LayoutProps): JSX.Element => {
         children
       )}
 
-      <Footer setIsAboutModalOpen={setIsAboutModalOpen} />
+      <Footer />
     </>
   );
 };
