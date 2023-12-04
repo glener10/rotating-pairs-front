f2efe0ec8dbc9fb2a06e77cb2ae12c843adf9b81
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2023-12-04 12:00:59 -0300

    feat: link to main page in the Header and changing the logo

--- src/pages/Layout/Header/index.tsx
+++ src/pages/Layout/Header/index.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { SimpleButton } from '@/components/atoms/SimpleButton';
+import Title from '@/components/atoms/Title';
 import useResponsive from '@/hooks/useResponsive';
 import { TBreakpoint } from '@/interfaces/TBreakpoint';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { Flex, Text } from '@radix-ui/themes';
+import Image from 'next/image';
+import router from 'next/router';
 import { Dispatch, SetStateAction } from 'react';
 
 type HeaderProps = {
@@ -25,16 +29,32 @@ export const Header = (props: HeaderProps): JSX.Element => {
   const breakpoint = useResponsive();
   const widthHeader = mappingWidthHeader(breakpoint);
 
+  async function goToHomePage(): Promise<void> {
+    await router.push('/');
+  }
+
+  const Logo = (): JSX.Element => (
+    <Image src="/iconDrawPairProgramming.svg" alt="Logo do seu site" width={40} height={40} />
+  );
+
   return (
     <header>
       <Flex
         style={{
           display: 'flex',
           alignItems: 'center',
-          justifyContent: 'flex-end',
+          justifyContent: 'space-between',
           padding: '24px',
         }}
       >
+        <Flex
+          style={{ display: 'flex', cursor: 'pointer' }}
+          onClick={async (): Promise<void> => goToHomePage()}
+        >
+          <Logo />
+          <Title>Rotating Pairs</Title>
+        </Flex>
+
         <Flex
           style={{
             display: 'flex',
